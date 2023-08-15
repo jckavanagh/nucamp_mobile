db212bb32749e28cf3b56eba35fbc55eaf294ce7
Jessica Kavanagh <kavanagh.jc@gmail.com>
2023-08-15 18:23:07 -0500

    CampsiteInfoScreen Edit

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View} from 'react-native';
+import { COMMENTS } from '../shared/comments';
 import RenderCampsite from "../features/campsites/RenderCampsite";
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />;
+
+    const [comments, setComments] = useState(COMMENTS);
+
+    const renderCommentItem = ({ item }) => {
+        return 
+    } 
+
+    return (
+        <FlatList
+            data={comments.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
+            renderItem-={renderCommentItem}
+        />
+    )
 };
 
 export default CampsiteInfoScreen;
+
