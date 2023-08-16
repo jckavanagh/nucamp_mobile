f28143b0e1da0ca0707b0ef70c8083fd1a44dfc3
Jessica Kavanagh <kavanagh.jc@gmail.com>
2023-08-16 14:45:02 -0500

    Exercise: Icons, Favorites, & Comments Part I

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -9,7 +9,15 @@ const CampsiteInfoScreen = ({ route }) => {
     const [comments, setComments] = useState(COMMENTS);
 
     const renderCommentItem = ({ item }) => {
-        return 
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+            </View>
+        )
     } 
 
     return (
@@ -17,10 +25,38 @@ const CampsiteInfoScreen = ({ route }) => {
             data={comments.filter(
                 (comment) => comment.campsiteId === campsite.id
             )}
-            renderItem-={renderCommentItem}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite campsite={campsite}/>
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
         />
     )
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    }
+});
 
 export default CampsiteInfoScreen;
 
