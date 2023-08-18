e4ba4754021a7e39e7553a839d8ef53cd2c0f4cb
Jessica Kavanagh <kavanagh.jc@gmail.com>
2023-08-17 21:07:00 -0500

    Exercise: Activity Indicator

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,13 +2,26 @@ import { ScrollView, Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
-const FeaturedItem = ({ item }) => {
+const FeaturedItem = (props) => {
+    const { item } = props;
+
+    if (props.isLoading) {
+        return <Loading />;
+    }
+    if (props.errMess) {
+        return (
+            <View>
+                <Text>{props.errMess}</Text>
+            </View>
+        );
+    }
     if (item) {
         return (
-            <Card containerStyle={{ padding: 0}}>
+            <Card containerStyle={{ padding: 0 }}>
                 <Card.Image source={{ uri: baseUrl + item.image }}>
-                    <View style={{ justifyContent: 'center', flex: 1}}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
                                 color: 'white',
@@ -22,11 +35,10 @@ const FeaturedItem = ({ item }) => {
                 </Card.Image>
                 <Text style={{ margin: 20 }}>{item.description}</Text>
             </Card>
-        )
+        );
     }
-    return <View />
-}
-
+    return <View />;
+};
 
 const HomeScreen = () => {
     const campsites = useSelector((state) => state.campsites);
@@ -34,16 +46,30 @@ const HomeScreen = () => {
     const partners = useSelector((state) => state.partners);
 
     const featCampsite = campsites.campsitesArray.find((item) => item.featured);
-    const featPromotion = promotions.promotionsArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
     const featPartner = partners.partnersArray.find((item) => item.featured);
 
     return (
         <ScrollView>
-            <FeaturedItem item={featCampsite} />
-            <FeaturedItem item={featPromotion} />
-            <FeaturedItem item={featPartner} />
+            <FeaturedItem
+                item={featCampsite}
+                isLoading={campsites.isLoading}
+                errMess={campsites.errMess}
+            />
+            <FeaturedItem
+                item={featPromotion}
+                isLoading={promotions.isLoading}
+                errMess={promotions.errMess}
+            />
+            <FeaturedItem
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
+            />
         </ScrollView>
-    )
+    );
 };
 
 export default HomeScreen;
