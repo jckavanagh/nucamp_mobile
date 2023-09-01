c263b86bc1e40fac7794ece71fdabdb9c399da77
Jessica Kavanagh <kavanagh.jc@gmail.com>
2023-09-01 15:01:28 -0500

    Exercise: Tab Navigation

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,41 +1,42 @@
 import { useEffect, useState } from 'react';
-import { View, Button, StyleSheet } from 'react-native';
-import { CheckBox, Input } from 'react-native-elements';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { CheckBox, Input, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const LoginScreen = () => {
-    const [ username, setUsername ] = useState('');
-    const [ password, setPassword ] = useState('');
-    const [ remember, setRemember ] = useState(false);
+const LoginTab = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
 
     const handleLogin = () => {
-        console.log('username: ', username);
-        console.log('password: ', password);
-        console.log('remember: ', remember);
-        if(remember){
+        console.log('username:', username);
+        console.log('password:', password);
+        console.log('remember:', remember);
+        if (remember) {
             SecureStore.setItemAsync(
-                'userinfo', 
-                JSON.stringify ({
-                username,
-                password
+                'userinfo',
+                JSON.stringify({
+                    username,
+                    password
                 })
-            ).catch((error) => console.log('Could not save user info', error))
+            ).catch((error) => console.log('Could not save user info', error));
         } else {
             SecureStore.deleteItemAsync('userinfo').catch((error) =>
                 console.log('Could not delete user info', error)
-            )
+            );
         }
-    }
+    };
 
     useEffect(() => {
         SecureStore.getItemAsync('userinfo').then((userdata) => {
             const userinfo = JSON.parse(userdata);
-            if(userinfo){
+            if (userinfo) {
                 setUsername(userinfo.username);
                 setPassword(userinfo.password);
                 setRemember(true);
             }
-        })
+        });
     }, []);
 
     return (
@@ -68,11 +69,87 @@ const LoginScreen = () => {
                     onPress={() => handleLogin()}
                     title='Login'
                     color='#5637DD'
+                    icon={
+                        <Icon
+                            name='sign-in'
+                            type='font-awesome'
+                            color='#fff'
+                            iconStyle={{ marginRight: 10 }}
+                        />
+                    }
+                    buttonStyle={{ backgroundColor: '#5637DD' }}
+                />
+            </View>
+            <View style={styles.formButton}>
+                <Button
+                    onPress={() => navigation.navigate('Register')}
+                    title='Register'
+                    type='clear'
+                    icon={
+                        <Icon
+                            name='user-plus'
+                            type='font-awesome'
+                            color='blue'
+                            iconStyle={{ marginRight: 10 }}
+                        />
+                    }
+                    titleStyle={{ color: 'blue' }}
                 />
             </View>
         </View>
-    )
-}
+    );
+};
+
+const RegisterTab = () => {
+    return <ScrollView></ScrollView>;
+};
+
+const Tab = createBottomTabNavigator();
+
+const LoginScreen = () => {
+    const tabBarOptions = {
+        activeBackgroundColor: '#5637DD',
+        inactiveBackgroundColor: '#CEC8FF',
+        activeTintColor: '#fff',
+        inactiveTintColor: '#808080',
+        labelStyle: { fontSize: 16 }
+    };
+
+    return (
+        <Tab.Navigator tabBarOptions={tabBarOptions}>
+            <Tab.Screen
+                name='Login'
+                component={LoginTab}
+                options={{
+                    tabBarIcon: (props) => {
+                        return (
+                            <Icon
+                                name='sign-in'
+                                type='font-awesome'
+                                color={props.color}
+                            />
+                        );
+                    }
+                }}
+            />
+            <Tab.Screen
+                name='Register'
+                component={RegisterTab}
+                options={{
+                    tabBarIcon: (props) => {
+                        return (
+                            <Icon
+                                name='user-plus'
+                                type='font-awesome'
+                                color={props.color}
+                            />
+                        );
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -81,17 +158,17 @@ const styles = StyleSheet.create({
     },
     formIcon: {
         marginRight: 10
-    }, 
+    },
     formInput: {
         padding: 10
     },
     formCheckbox: {
         margin: 10,
-        backgroundColor: null,
+        backgroundColor: null
     },
     formButton: {
         margin: 40
     }
-})
+});
 
-export default LoginScreen
+export default LoginScreen;
